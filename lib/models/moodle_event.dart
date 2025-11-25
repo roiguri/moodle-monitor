7cd2cccc46f5eee3d4b650e9be2e2e5a5c942950
Roi Guri <roiguri@mail.tau.ac.il>
2025-11-25 20:47:38 +0200

    feature: replace names with shortnames

--- lib/models/moodle_event.dart
+++ lib/models/moodle_event.dart
@@ -1,20 +1,25 @@
-
 class MoodleEvent {
   final String name;
   final String course;
   final int timeSort;
+  final String courseViewUrl;
+  final String url;
 
   MoodleEvent({
     required this.name,
     required this.course,
     required this.timeSort,
+    required this.courseViewUrl,
+    required this.url,
   });
 
   factory MoodleEvent.fromJson(Map<String, dynamic> json) {
     return MoodleEvent(
-      name: json['name'] as String,
-      course: json['course']['fullname'] as String,
+      name: json['activityname'] as String,
+      course: json['course']['shortname'] as String,
       timeSort: json['timesort'] as int,
+      courseViewUrl: json['course']['viewurl'] as String,
+      url: json['url'] as String,
     );
   }
 }
